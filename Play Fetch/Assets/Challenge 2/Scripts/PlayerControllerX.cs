9780d06125cef6987ef452d29406cb4f02f7eea8
Hauwa Salihu <hauwasalih1010@gmail.com>
2022-10-06 18:31:55 +0100

    First commit
    Play fetch source code added to repository.

---
+++ Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+        //Declare variables spawning dogs at an interval
+    
+    private float spawnDog = 1;
+
+    private float start = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog and check too see that the start time
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > start)
+        {
+            //Reset the start dog spawn time
+
+            start = Time.time + spawnDog;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+}
